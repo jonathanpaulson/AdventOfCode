c7920a6145663fbe06d5b2d17eaa738461536bc4
Jonathan Paulson <jonathanpaulson@gmail.com>
2020-12-17 00:35:35 -0500

    Day 17: Only check the neighbors of some cell that is on.
    ~2x speedup

--- 2020/17.py
+++ 2020/17.py
@@ -14,23 +14,29 @@ def solve(p1):
 
     for _ in range(6):
         NEW_ON = set()
+        CHECK = set()
+        for (x,y,z,w) in ON:
+            for dx in [-1,0,1]:
+                for dy in [-1,0,1]:
+                    for dz in [-1,0,1]:
+                        for dw in [-1,0,1]:
+                            if w+dw==0 or (not p1):
+                                CHECK.add((x+dx, y+dy, z+dz, w+dw))
+
         w_range = ([0] if p1 else range_for(3, ON))
-        for x in range_for(0, ON):
-            for y in range_for(1, ON):
-                for z in range_for(2, ON):
-                    for w in w_range:
-                        nbr = 0
-                        for dx in [-1,0,1]:
-                            for dy in [-1,0,1]:
-                                for dz in [-1,0,1]:
-                                    for dw in [-1,0,1]:
-                                        if dx!=0 or dy!=0 or dz!=0 or dw!=0:
-                                            if (x+dx, y+dy, z+dz, w+dw) in ON:
-                                                nbr += 1
-                        if (x,y,z,w) not in ON and nbr == 3:
-                            NEW_ON.add((x,y,z,w))
-                        if (x,y,z,w) in ON and nbr in [2,3]:
-                            NEW_ON.add((x,y,z,w))
+        for (x,y,z,w) in CHECK:
+            nbr = 0
+            for dx in [-1,0,1]:
+                for dy in [-1,0,1]:
+                    for dz in [-1,0,1]:
+                        for dw in [-1,0,1]:
+                            if dx!=0 or dy!=0 or dz!=0 or dw!=0:
+                                if (x+dx, y+dy, z+dz, w+dw) in ON:
+                                    nbr += 1
+            if (x,y,z,w) not in ON and nbr == 3:
+                NEW_ON.add((x,y,z,w))
+            if (x,y,z,w) in ON and nbr in [2,3]:
+                NEW_ON.add((x,y,z,w))
         ON = NEW_ON
 
     return len(ON)
