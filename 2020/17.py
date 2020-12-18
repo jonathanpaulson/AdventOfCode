05271340e3311668b57b51ed5b18747ae169d3e0
Jonathan Paulson <jonathanpaulson@gmail.com>
2020-12-17 23:34:55 -0500

    itertools.product

--- 2020/17.py
+++ 2020/17.py
@@ -1,5 +1,6 @@
 import fileinput
 import re
+import itertools
 
 def solve(p1):
     ON = set()
@@ -13,22 +14,16 @@ def solve(p1):
         NEW_ON = set()
         CHECK = set()
         for (x,y,z,w) in ON:
-            for dx in [-1,0,1]:
-                for dy in [-1,0,1]:
-                    for dz in [-1,0,1]:
-                        for dw in [-1,0,1]:
-                            if w+dw==0 or (not p1):
-                                CHECK.add((x+dx, y+dy, z+dz, w+dw))
+            for dx,dy,dz,dw in itertools.product([-1,0,1], repeat=4):
+                if w+dw==0 or (not p1):
+                    CHECK.add((x+dx, y+dy, z+dz, w+dw))
 
         for (x,y,z,w) in CHECK:
             nbr = 0
-            for dx in [-1,0,1]:
-                for dy in [-1,0,1]:
-                    for dz in [-1,0,1]:
-                        for dw in [-1,0,1]:
-                            if dx!=0 or dy!=0 or dz!=0 or dw!=0:
-                                if (x+dx, y+dy, z+dz, w+dw) in ON:
-                                    nbr += 1
+            for dx,dy,dz,dw in itertools.product([-1,0,1], repeat=4):
+                if dx!=0 or dy!=0 or dz!=0 or dw!=0:
+                    if (x+dx, y+dy, z+dz, w+dw) in ON:
+                        nbr += 1
             if (x,y,z,w) not in ON and nbr == 3:
                 NEW_ON.add((x,y,z,w))
             if (x,y,z,w) in ON and nbr in [2,3]:
