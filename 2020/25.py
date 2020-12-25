5295f00722fb2c1c93c75c0b6d020b964ad80474
Jonathan Paulson <jonathanpaulson@gmail.com>
2020-12-25 00:12:09 -0500

    cleanup

--- 2020/25.py
+++ 2020/25.py
@@ -3,18 +3,7 @@ import sys
 
 
 def transform(x, sz):
-    #if sz == 0:
-    #    return 1
-    #elif sz % 2 == 0:
-    #    return transform((x*x)%20201227, sz/2)
-    #else:
-    #    return (x*transform(x, sz-1))%20201227
     return pow(x, sz, 20201227)
-    #v = 1
-    #for _ in range(sz):
-    #    v = v*x
-    #    v = v%20201227
-    #return v
 
 k1,k2 = [int(x) for x in sys.stdin.read().strip().split('\n')]
 #k1,k2 = 5764801,17807724
