215f18d7d5b9761ec181954d2e62b6fed3bd12f5
jonathanpaulson <jonathanpaulson@gmail.com>
2022-12-25 00:39:11 -0500

    Add directly in SNAFU

--- 2022/25.py
+++ 2022/25.py
@@ -8,6 +8,7 @@ infile = sys.argv[1] if len(sys.argv)>1 else '25.in'
 data = open(infile).read().strip()
 lines = [x for x in data.split('\n')]
 
+# Solution 1: Convert to base 10, add, then convert back
 def to_base10(s):
   ans = 0
   base = 1
@@ -33,10 +34,41 @@ def to_snafu(n,p5):
       return D[d]+to_snafu(n-p5*d, p5//5)
   assert False, (n, p5,n//p5)
 
-ans = 0
+base10_sum = 0
 for line in lines:
-  ans += to_base10(line)
+  base10_sum += to_base10(line)
 p5 = 1
-while abs(ans)>max_value(p5):
+while abs(base10_sum)>max_value(p5):
   p5 *= 5
-print(to_snafu(ans,p5))
+print(to_snafu(base10_sum,p5))
+
+# Solution 2: add directly in SNAFU
+max_len = 0
+for line in lines:
+  max_len = max(max_len, len(line))
+
+VD = {'2': 2, '1': 1, '0': 0, '-': -1, '=': -2}
+D = {2: '2', 1: '1', 0: '0', -1: '-', -2:'='}
+ans = ''
+my_base10 = 0
+carry = 0
+for i in range(max_len):
+  sum_i = carry
+  for line in lines:
+    if i<len(line):
+      sum_i += VD[line[len(line)-1-i]]
+  carry = 0
+  while sum_i >= 3:
+    carry += 1
+    sum_i -= 5
+  while sum_i <= -3:
+    carry -= 1
+    sum_i += 5
+  assert -2<=sum_i<=2
+  ans += D[sum_i]
+  my_base10 += sum_i*5**i
+assert carry == 0
+
+final = (''.join(list(reversed(ans))))
+assert to_base10(final) == base10_sum
+print(final)
