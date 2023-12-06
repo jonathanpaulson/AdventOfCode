58906b75ee8b62861178d7905ccb423a45e59287
Jonathan Paulson <jonathanpaulson@gmail.com>
2023-12-06 00:04:48 -0500

    2023 day 6

---
+++ 2023/6.py
@@ -0,0 +1,33 @@
+import sys
+import re
+from collections import defaultdict
+D = open(sys.argv[1]).read().strip()
+L = D.split('\n')
+ans = 0 
+
+t,d = L
+times = [int(x) for x in t.split(':')[1].split()]
+dist = [int(x) for x in d.split(':')[1].split()]
+
+T = ''
+for t in times:
+  T += str(t)
+T = int(T)
+D = ''
+for d in dist:
+  D += str(d)
+D = int(D)
+
+def f(t, d):
+  ans = 0
+  for x in range(t+1):
+    dx = x*(t-x)
+    if dx>=d:
+      ans += 1
+  return ans
+
+ans = 1
+for i in range(len(times)):
+  ans *= f(times[i], dist[i])
+print(ans)
+print(f(T,D))
